171cb32b1470815de26d55a375fb35d4fa86b10f
Zhijie He <hez@garena.com>
2015-10-26 22:41:06 +0800

    add test.

--- main.cpp
+++ main.cpp
@@ -6,23 +6,61 @@
 
 using namespace std;
 
-int main()
+void test_kv(SSDBClient &client)
 {
-    cout << "fuck" << endl;
-	
+	string key("test_key");
+	string value("hello_ssdb");
+	Status s = client.set(key, value);
+	if (!s.ok())
+	{
+		std::cout << "set fail" << std::endl;
+		return;
+	}
+	string expectValue;
+	s = client.get(key, &expectValue);
+	if (!s.ok() || s.not_found())
+	{
+		std::cout << "get fail" << std::endl;
+		return;
+	}
+	if (expectValue != value)
+	{
+		std::cout << "get value not the same" << std::endl;
+		return;
+	}
+}
+
+void test_hash(SSDBClient &client)
+{
+	string key("test_hash");
+	string subkey("subkey1");
+	string value("hello hash");
+	Status s = client.hset(key, subkey, value);
+	if (!s.ok())
+	{
+		std::cout << "hset fail" << std::endl;
+		return;
+	}
+	string expectValue;
+	s = client.hget(key, subkey, &expectValue);
+	if (!s.ok() || s.not_found())
+	{
+		std::cout << "hget fail" << std::endl;
+		return;
+	}
+	if (expectValue != value)
+	{
+		std::cout << "hget value not the same" << std::endl;
+		return;
+	}
+}
+
+int main()
+{	
 	SSDBClient client;
 	client.connect("203.116.50.232", 8888);
-	Status s = client.set("abc", "cdefg");
-	if (!s.ok())
-	{
-		cout << "what the hell" << std::endl;
-	}
-	std::string val;
-	s = client.get("abc", &val);
-	if (!s.ok())
-	{
-		cout << "not ok" << endl;
-	}
-	cout << val << endl;
+	test_kv(client);
+	test_hash(client);
+
     return 0;
 }
